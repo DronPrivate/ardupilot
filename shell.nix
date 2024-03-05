6a904f8ac04b149e193c9bb31622d2178c288c2d
sorki <srk@48.io>
2024-03-05 13:37:33 +0100

    shell.nix: use empy 3.3.4

--- shell.nix
+++ shell.nix
@@ -1,4 +1,22 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (self: super: {
+      python3 = super.python3.override {
+        packageOverrides = pself: psuper: {
+          empy = psuper.buildPythonPackage rec {
+            pname = "empy";
+            version = "3.3.4";
+            src = super.fetchPypi {
+              inherit pname version;
+              sha256 = "c6xJeFtgFHnfTqGKfHm8EwSop8NMArlHLPEgauiPAbM=";
+            };
+          };
+        };
+      };
+    })
+  ];
+  }
+}:
 pkgs.stdenv.mkDerivation {
   name = "ArduPilot-shell";
   buildInputs = with pkgs; [
