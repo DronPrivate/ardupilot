411b80bfb48d890ee146871fac0793056df53a64
97frantarusnak <97frantarusnak@gmail.com>
2024-12-10 12:20:58 +0100

    synchro

--- build/Pix32v5/ap_version.h
+++ build/Pix32v5/ap_version.h
@@ -6,7 +6,7 @@
 #error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
 #endif
 
-#define GIT_VERSION "70ab446b"
-#define GIT_VERSION_INT 1890272363
+#define GIT_VERSION "6d8a8fc1"
+#define GIT_VERSION_INT 1837797313
 #define AP_BUILD_ROOT "/home/malina01/ardupilot"
 #define CHIBIOS_GIT_VERSION "88b84600"
