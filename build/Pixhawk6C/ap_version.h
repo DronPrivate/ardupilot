4810f693a54af69a44c759daa465e495e8d7cd1d
97frantarusnak <97frantarusnak@gmail.com>
2025-01-06 11:44:32 +0100

    rebuild Pixhawk6C

---
+++ build/Pixhawk6C/ap_version.h
@@ -0,0 +1,12 @@
+// auto-generated header, do not edit
+
+#pragma once
+
+#ifndef FORCE_VERSION_H_INCLUDE
+#error ap_version.h should never be included directly. You probably want to include AP_Common/AP_FWVersion.h
+#endif
+
+#define GIT_VERSION "3fc35d18"
+#define GIT_VERSION_INT 1069767960
+#define AP_BUILD_ROOT "/home/malina01/ardupilot"
+#define CHIBIOS_GIT_VERSION "88b84600"
